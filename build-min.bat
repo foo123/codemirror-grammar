45c9383f5536cb65a8cb768b2cdb690c5afc9731
Nikos M <nikosms0@gmail.com>
2015-01-13 19:58:46 +0200

    update buildtools

--- build-min.bat
+++ build-min.bat
@@ -3,15 +3,15 @@
 REM ###################################################
 REM #
 REM #   The buildtools repository is at:
-REM #   https://github.com/foo123/scripts/buildtools
+REM #   https://github.com/foo123/Beeld
 REM #
 REM ###################################################
 
 REM to use the python build tool do:
-REM python %BUILDTOOLS%\build.py --deps ".\dependencies-min"
+REM python %BUILDTOOLS%\Beeld.py --config ".\config.custom" --tasks minify
 
 REM to use the php build tool do:
-REM php -f %BUILDTOOLS%\build.php -- --deps=".\dependencies-min"
+REM php -f %BUILDTOOLS%\Beeld.php --  --config=".\config.custom" --tasks=minify
 
 REM to use the node build tool do:
-node %BUILDTOOLS%\build.js --deps ".\dependencies-min"
+node %BUILDTOOLS%\Beeld.js  --config ".\config.custom" --tasks minify
