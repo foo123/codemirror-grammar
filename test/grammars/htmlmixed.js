3f4770123ad81e69922d28b44926e08428ca1dd4
Nikos M <nikosms0@gmail.com>
2016-04-09 18:23:32 +0300

    v. 4.0.0 in progress

---
+++ test/grammars/htmlmixed.js
@@ -0,0 +1,76 @@
+// 1. a partial html grammar with mixed javascript/css sub-grammars in simple JSON format
+var htmlmixed_grammar = {
+    
+// prefix ID for regular expressions, represented as strings, used in the grammar
+"RegExpID"                          : "RE::",
+
+"Extra"                             : {
+    
+    "fold"                          : "xml"
+    //"electricChars"               : "<"
+    
+},
+    
+// Style model
+"Style"                             : {
+
+     "comment"                      : "comment"
+    ,"declaration"                  : "tag"
+    ,"doctype"                      : "meta"
+    ,"meta"                         : "meta"
+    ,"cdata"                        : "atom"
+    ,"tag"                          : "tag"
+    ,"attribute"                    : "attribute"
+    ,"string"                       : "string"
+    ,"atom"                         : "atom"
+    ,"number"                       : "number"
+    ,"error"                        : "error"
+    
+},
+
+// Lexical model
+"Lex"                               : {
+     
+     "comment:comment"              : ["<!--", "-->"]
+    ,"declaration:block"            : ["<?xml", "?>"]
+    ,"doctype:block"                : ["RE::/<!doctype\\b/i", ">"]
+    ,"meta:block"                   : ["RE::/<\\?[_a-zA-Z][\\w\\._\\-]*/", "?>"]
+    ,"cdata:block"                  : ["<![CDATA[", "]]>"]
+    ,"open_tag"                     : "RE::/<([_a-zA-Z][_a-zA-Z0-9\\-]*)/"
+    ,"close_tag"                    : "RE::/<\\/([_a-zA-Z][_a-zA-Z0-9\\-]*)>/"
+    ,"attribute"                    : "RE::/[_a-zA-Z][_a-zA-Z0-9\\-]*/"
+    ,"string:line-block"            : [["\""], ["'"]]
+    ,"number"                       : ["RE::/[0-9]\\d*/", "RE::/#[0-9a-fA-F]+/"]
+    ,"atom"                         : ["RE::/&#x[a-fA-F\\d]+;/", "RE::/&#[\\d]+;/", "RE::/&[a-zA-Z][a-zA-Z0-9]*;/"]
+    ,"text"                         : "RE::/[^<&]+/"
+    
+    // actions
+    ,"tag_ctx:action"               : {"context":true}
+    ,"\\tag_ctx:action"             : {"context":false}
+    ,"unique_id:action"             : {"unique":["xml", "$1"],"msg":"Duplicate id value \"$0\""}
+    ,"unique_att:action"            : {"unique":["tag", "$0"],"msg":"Duplicate attribute \"$0\"","in-context":true}
+    ,"tag_opened:action"            : {"push":"<$1>","ci":true}
+    ,"tag_closed:action"            : {"pop":"<$1>","ci":true,"msg":"Tags \"$0\" and \"$1\" do not match"}
+    ,"tag_autoclosed:action"        : {"pop":null}
+    ,"out_of_place:error"           : "\"$2$3\" can only be at the beginning of XML document"
+    
+},
+    
+// Syntax model (optional)
+"Syntax"                            : {
+     
+     "javascript"                   : {"subgrammar":"javascript"}
+    ,"css"                          : {"subgrammar":"css"}
+    ,"tag_att"                      : "'id'.attribute unique_att '=' string unique_id | attribute unique_att '=' (string | number)"
+    ,"style_tag"                    : "'<style>'.tag css '</style>'.tag"
+    ,"script_tag"                   : "'<script>'.tag javascript '</script>'.tag"
+    ,"start_tag"                    : "open_tag.tag tag_ctx tag_opened tag_att* ('>'.tag | '/>'.tag tag_autoclosed) \\tag_ctx"
+    ,"end_tag"                      : "close_tag.tag tag_closed"
+    ,"htmlmixed"                    : "(^^1 declaration? doctype?) (declaration.error out_of_place | doctype.error out_of_place | comment | meta | cdata | style_tag | script_tag | start_tag | end_tag | atom | text)*"
+    
+},
+    
+// what to parse and in what order
+"Parser"                            : [ ["htmlmixed"] ]
+
+};
